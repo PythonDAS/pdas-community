d6d7e3574cd4f121215d66658d26dd7245e1fc02
devarchi33 <devarchi33@gmail.com>
2016-04-02 12:41:01 +0900

    1. thyme leaf, webjar 를 이용한 adminlte, jsp 사용을 위한 tomcat-embed-jasper 의존성 추가. 2. MvcConfig 설정 제거. 3. application.yml 추가.

--- src/main/java/com/devarchi/web/config/MvcConfig.java
+++ src/main/java/com/devarchi/web/config/MvcConfig.java
@@ -1,51 +1,50 @@
-package com.devarchi.web.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-/**
- * Created by donghoon on 2016. 2. 14..
- */
-@EnableWebMvc
-@Configuration
-@ComponentScan(
-        basePackages = "com.devarchi.web",
-        includeFilters = @ComponentScan.Filter({Controller.class, RestController.class, Service.class, Repository.class})
-)
-public class MvcConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
-    }
-
-    /**
-     * InternalResouceViewResolver 는 view 가 있으면 다른 resolver 를 검색하지 않기 때문에, 최후순위로 등록한다
-     *
-     * @return
-     */
-    @Bean
-    public ViewResolver htmlViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/view/");
-        resolver.setSuffix(".jsp");
-        resolver.setOrder(2);
-        return resolver;
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-}
+//package com.devarchi.web.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.servlet.ViewResolver;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//
+///**
+// * Created by donghoon on 2016. 2. 14..
+// */
+//@EnableWebMvc
+//@Configuration
+//@ComponentScan(
+//        basePackages = "com.devarchi.web",
+//        includeFilters = @ComponentScan.Filter({Controller.class, RestController.class, Service.class, Repository.class})
+//)
+//public class MvcConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
+//    }
+//
+//    /**
+//     * InternalResouceViewResolver 는 view 가 있으면 다른 resolver 를 검색하지 않기 때문에, 최후순위로 등록한다
+//     *
+//     * @return
+//     */
+////    @Bean
+////    public ViewResolver htmlViewResolver() {
+////        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+////        resolver.setPrefix("/WEB-INF/view/");
+////        resolver.setSuffix(".jsp");
+////        resolver.setOrder(2);
+////        return resolver;
+////    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
+//}
